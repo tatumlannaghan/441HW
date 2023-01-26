13d672acdd23bab4cdc67ac8765c8d6270af10e0
tatumlannaghan <77710274+tatumlannaghan@users.noreply.github.com>
2023-01-25 19:55:15 -0700

    first try?

---
+++ hw2/script.js
@@ -0,0 +1,11 @@
+/*
+Author: Tatum Lannaghan
+Date: 1/25/23
+
+Questions to answer: 3 of your favorite websites
+3 of your favorite games if you play any - they can be board games, etc.
+3 of your favorite artists and their cultural importance
+*/
+
+console.log("My favorite websites are twitch.tv, youtube.com, and pinterest.com. My favorite games are Mario Party Superstars, The Last of Us and Clue. My favorite artists are Joe Wright for making Pride and Prejudice (2006), Stephen Hillenburg and Jason Shiga for creating Spongebob Squarepants, and Bo Burnham for creating Inside. All of which have heavily impacted my life resulting in my cultural importance.")
+
