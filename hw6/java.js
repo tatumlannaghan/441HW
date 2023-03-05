8698988dd6c4a32db7416ccdbaf72956261f2b58
tatumlannaghan <77710274+tatumlannaghan@users.noreply.github.com>
2023-03-05 13:40:27 -0700

    all that jazz

--- hw6/java.js
+++ hw6/java.js
@@ -1,4 +1,5 @@
 var imageNames = ["image1", "image2", "image3", "image4"];
+//blank variable
 var blankImagePath = "images/boot.jpg";
 var firstNumber = -1;
 var secondNumber = -1;
@@ -6,19 +7,15 @@ var score = 0;
 var allFound = 0;
 // JSON declaration
 var player = {"firstname":"", "lastname":"", "age":0, "score":0};
-
-// create a variable with the blank image name
-// create a empty array for the actual images
+// actual images
 var actualImages = new Array();
     
 function printBlanks()
 {
-   // call our random image creation function
     createRandomImageArray();
-    // create a for loop
+    // for loop
     for(var i = 0; i < imageNames.length; i++)
     {
-    // iterate through the image tag ids and sets the source 
         document.getElementById(imageNames[i]).src= blankImagePath;
     }
          
@@ -26,21 +23,17 @@ function printBlanks()
 
 function createRandomImageArray()
 {
-    // create an array of actual images
-    var actualImagePath = ["images/cowboy.jpg", "images/shoe.jpeg"];
-    // create another array to make sure the images only get added twice
+    var actualImagePath = ["images/cowboy.jpg", "images/shoe.jpg"];
     var count = [0,0];
-    // create a while statement to check to see if our actual image array is full
+    // while statement
     while(actualImages.length < 4)
     {
-        // get a random number between 0 and the number total number of images that we can choose from
+        // random 
         var randomNumber = Math.floor(Math.random() * actualImagePath.length)
-          // create an if statement that says if the total number added is less than 2, then
-        // add the image to the actual image array
+    
         if(count[randomNumber] < 2)
         {
             actualImages.push(actualImagePath[randomNumber]);
-            // then add one to the array that makes sure only two images can be added
             count[randomNumber] = count[randomNumber] + 1;
         }
     }   
@@ -49,27 +42,29 @@ function createRandomImageArray()
 function flipImage(number)
 {
     
-    // make the second image appear
+    // second image appear
     if(firstNumber >= 0)
     {
         secondNumber = number;
         document.getElementById(imageNames[number]).src = actualImages[secondNumber];
         
     }
-    else if(firstNumber < 0) // make the first image appear
+    else if(firstNumber < 0) 
+    // first image appear
     {
         firstNumber = number;
         document.getElementById(imageNames[firstNumber]).src= actualImages[firstNumber];
     
     }
 
-    // check to see if the images do not match
+    //if the images do not match
     if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
     {
         score++;
-        setTimeout(imagesDisappear, 1000); // calls a method after 1 second
+        setTimeout(imagesDisappear, 1000); 
+        // calls a method 
     }
-    // check to see if the images do match
+    // if the images do match
     else if(actualImages[secondNumber] == actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
     {
         score++;
@@ -110,7 +105,7 @@ function addToPlayer()
     window.location = "index.html";
 }
 
-// get the information out of JSON
+// JSON info
 function playerInfo()
 {
     var playerInformation = localStorage.getItem("playerInfo");
