402922e70af598393c561028a7acd092b21a09cb
tatumlannaghan <77710274+tatumlannaghan@users.noreply.github.com>
2023-05-10 19:07:30 -0600

    oh yea

---
+++ final/java.js
@@ -0,0 +1,13 @@
+//joke
+function testResults (form) {
+    var inputValue = form.inputbox.value;
+    alert ("lol");
+}
+//close form
+$(document).ready(function(){
+    $("button").click(function(){
+      $("form").toggle(500)
+      });
+    });
+
+ 
